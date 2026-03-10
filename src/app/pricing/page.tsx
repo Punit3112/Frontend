ad219768e6ed49640cc2bdbc792f2a72e625a3f3
Punit Tripathi <96485246+Punit3112@users.noreply.github.com>
2026-03-10 07:16:55 +0000

    Add footer to website

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,15 +1,19 @@
-import Hero from '../components/Pricing/HeroHolder'
-import BookDemo from '../components/BookDemo'
-import Plans from '../components/Plans'
-import CallToAction from '../components/CTA'
+import Hero from "../components/Pricing/HeroHolder";
+import BookDemo from "../components/BookDemo";
+import Plans from "../components/Plans";
+import CallToAction from "../components/CTA";
+import Footer from "../components/Footer";
 
-export default function PricingPage(){
-    return <div className="content">
-        <Hero />
-        <section className='my-[2%] mx-auto'>
-            <BookDemo/>
-            <Plans />
-            <CallToAction />
-        </section>
+export default function PricingPage() {
+  return (
+    <div className="content">
+      <Hero />
+      <section className="my-[2%] mx-auto">
+        <BookDemo />
+        <Plans />
+        <CallToAction />
+      </section>
+      <Footer />
     </div>
-} 
+  );
+}
