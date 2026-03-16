de8caa5e6bbc6421708c515764d676decd66aa3e
Punit3112 <punittripathi100@gmail.com>
2026-03-16 10:30:54 +0530

    Add styling to components

--- src/app/components/CreatorSection.jsx
+++ src/app/components/CreatorSection.jsx
@@ -4,14 +4,14 @@ import CreatorCard from "../components/CreatorCard"
 export default function CreatorSection({ title, subtitle, seeAllHref, creators }) {
   return (
     <div className="section-holder">
-      <h2 className="section-title">{title}</h2>
-      <h3 className="section-subtitle">
+      <h2 className="flex items-baseline text-[150%] font-semibold m-0">{title}</h2>
+      <h3 className="flex items-center my-[4px] mb-[8px] text-[92%] font-normal text-gray-500">
         {subtitle}
-        <span className="explore-btn" onClick={() => location.href = seeAllHref}>
+        <span className="text-[#222] ml-auto cursor-pointer font-semibold" onClick={() => location.href = seeAllHref}>
           See All
         </span>
       </h3>
-      <div className="listing-row">
+      <div className="flex justify-between mt-[1%] max-[1150px]:overflow-x-scroll max-[1150px]:mt-[4%]">
         {creators.map(creator => (
           <CreatorCard key={creator.id} creator={creator} />
         ))}
